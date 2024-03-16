90dcd98577489c39665e13f535dfbadffb29ef73
Daniel Apatin <daniel@apatin.ru>
2024-03-16 13:07:57 +0300

    process polygons instead of rectangles

--- main.go
+++ main.go
@@ -8,26 +8,33 @@ import (
 	"image/draw"
 	"image/jpeg"
 	"io/ioutil"
-	"log"
 	"mime/multipart"
 	"net/http"
-	"os"
+	"regexp"
 	"strconv"
+	"time"
 
+	"github.com/ad/go-parking/poly"
 	"github.com/ernyoke/imger/edgedetection"
 	"github.com/ernyoke/imger/effects"
 	"github.com/ernyoke/imger/grayscale"
 	"github.com/ernyoke/imger/resize"
-	"github.com/ernyoke/imger/transform"
 	"github.com/ernyoke/imger/utils"
-
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/basicfont"
-	"golang.org/x/image/math/fixed"
-
-	"github.com/llgcode/draw2d/draw2dimg"
-	"github.com/llgcode/draw2d/draw2dkit"
 )
+
+var formTemplate = `
+<html>
+<body>
+<form action="/process" method="post" enctype="multipart/form-data">
+<input type="text" name="target" value="${target}" placeholder="target">
+<input type="text" name="token" value="${token}" placeholder="token">
+<input type="checkbox" name="day" value="${day}" checked> is day
+<input type="file" name="file" />
+<input type="submit" value="Upload" />
+</form>
+</body>	
+</html>
+`
 
 func main() {
 	mux := http.NewServeMux()
@@ -35,20 +42,7 @@ func main() {
 	// return form for uploading image
 	mux.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("get form...")
-		w.Write([]byte(`
-		<html>
-		<body>
-		<form action="/process" method="post" enctype="multipart/form-data">
-		<input type="text" name="target" placeholder="target">
-		<input type="text" name="token" placeholder="token">
-		<input type="checkbox" name="day" value="1"> is day
-		<input type="checkbox" name="update" value="1"> update
-		<input type="file" name="file" />
-		<input type="submit" value="Upload" />
-		</form>
-		</body>	
-		</html>
-		`))
+		w.Write([]byte(formatForm(r)))
 	})
 
 	mux.HandleFunc("/process", processImage)
@@ -59,13 +53,25 @@ func main() {
 }
 
 func processImage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(formatForm(r)))
+
+	resizeScale := 0.5
+
+	start := time.Now()
 	fmt.Println("Processing image...")
 
-	// isDay := true
+	isDay := true
 
-	// if r.FormValue("day") == "1" {
-	// 	isDay = true
-	// }
+	if r.FormValue("day") == "1" {
+		isDay = true
+	}
+
+	tresholdEmpty := 94.0
+	tresholdEdges := 128.0
+	if isDay {
+		tresholdEmpty = 96.0
+		tresholdEdges = 192.0
+	}
 
 	// get file from form
 	file, _, err := r.FormFile("file")
@@ -86,153 +92,82 @@ func processImage(w http.ResponseWriter, r *http.Request) {
 	imgRGBA := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
 	draw.Draw(imgRGBA, imgRGBA.Bounds(), img, b.Min, draw.Src)
 
-	// Rotate image
-	imgRGBA, errRotate := transform.RotateRGBA(imgRGBA, 20, image.Point{img.Bounds().Dx() / 2, img.Bounds().Dy() / 2}, true)
-	if errRotate != nil {
-		log.Fatal("Could not rotate image", errRotate)
+	grayscaleImg := grayscale.Grayscale(imgRGBA)
+	grayscaleImg, err = effects.SharpenGray(grayscaleImg)
+	if err != nil {
+		fmt.Printf("could not sharpen image: %s", err)
+
+		return
 	}
 
-	imgResized, errResize := resize.ResizeRGBA(imgRGBA, 0.8, 0.8, resize.InterNearest)
-	if errResize != nil {
-		log.Fatal("Could not resize image", errResize)
-	}
+	if resizeScale != 1.0 {
+		// Resize image to half size for faster processing
+		grayscaleImg, err = resize.ResizeGray(grayscaleImg, 0.5, 0.5, resize.InterNearest)
+		if err != nil {
+			fmt.Printf("could not resize image: %s", err)
 
-	grayscaleImg := grayscale.Grayscale(imgResized)
+			return
+		}
+	}
 
 	// Edge detection
-	imgEdges, errCanny := edgedetection.CannyGray(grayscaleImg, 1, 192, 1)
-	// imgEdges, errCanny := edgedetection.CannyGray(grayscaleImg, 32, 255, 1)
-	// imgEdges, errCanny := edgedetection.SobelGray(grayscaleImg, 1)
-	// imgEdges, errCanny := edgedetection.LaplacianGray(grayscaleImg, padding.BorderReplicate, edgedetection.K8)
+	imgEdges, errCanny := edgedetection.CannyGray(grayscaleImg, 1, tresholdEdges, 1)
+
 	if errCanny != nil {
-		log.Fatal("Could not detect edges", errCanny)
+		fmt.Printf("could not detect edges: %s", errCanny)
+		return
 	}
 
 	// Invert image
 	imgEdges = effects.InvertGray(imgEdges)
 
-	cropRect := image.Rect(550, 514, 1524, 1454)
+	emptyPixel := color.Gray{Y: 0xff}
 
-	// Crop
-	croppedEdges := imgEdges.SubImage(cropRect)
+	utils.ParallelForEachPixel(grayscaleImg.Bounds().Size(), func(x int, y int) {
+		for _, polygon := range polygons {
+			point := poly.XY{X: float64(x) / resizeScale, Y: float64(y) / resizeScale}
+			if point.In(*polygon) {
+				// r, g, _, _ := img.At(x, y).RGBA()
+				// img.Set(x, y, color.RGBA{uint8(r) + 10, uint8(g) + 10, 255, 255})
 
-	// Crop
-	croppedImg := imgResized.SubImage(cropRect)
-
-	b = croppedImg.Bounds()
-	output := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
-	draw.Draw(output, output.Bounds(), croppedImg, b.Min, draw.Src)
-
-	c := croppedEdges.Bounds()
-	edgesImg := image.NewRGBA(image.Rect(0, 0, c.Dx(), c.Dy()))
-	draw.Draw(edgesImg, edgesImg.Bounds(), croppedEdges, c.Min, draw.Src)
-
-	checkRects := []image.Rectangle{
-		image.Rect(195, 248, 224, 308),
-		image.Rect(241, 248, 270, 308),
-		image.Rect(283, 248, 310, 308),
-		image.Rect(323, 248, 351, 308),
-		image.Rect(373, 268, 447, 306),
-		image.Rect(334, 369, 402, 412),
-		image.Rect(170, 400, 208, 475),
-		image.Rect(213, 400, 244, 475),
-		image.Rect(250, 400, 278, 475),
-		image.Rect(500, 288, 584, 337),
-		image.Rect(510, 338, 580, 381),
-		image.Rect(234, 583, 312, 618),
-		image.Rect(338, 564, 415, 616),
-		image.Rect(839, 55, 877, 128),
-		image.Rect(840, 140, 878, 217),
-		image.Rect(765, 240, 847, 277),
-		image.Rect(770, 278, 851, 318),
-		image.Rect(783, 319, 863, 369),
-		image.Rect(783, 359, 863, 415),
-		image.Rect(783, 415, 863, 475),
-		image.Rect(916, 57, 955, 126),
-		image.Rect(922, 127, 956, 209),
-		image.Rect(923, 223, 957, 290),
-		image.Rect(924, 294, 958, 376),
-		image.Rect(925, 381, 961, 462),
-		image.Rect(526, 432, 595, 514),
-		image.Rect(510, 384, 595, 448),
-		image.Rect(559, 477, 625, 555),
-		image.Rect(425, 660, 497, 710),
-		image.Rect(206, 732, 246, 812),
-		image.Rect(202, 822, 242, 882),
-		image.Rect(44, 786, 88, 866),
-		image.Rect(333, 661, 420, 703),
-		image.Rect(3, 785, 41, 864),
-		image.Rect(79, 878, 141, 919),
-		image.Rect(442, 393, 470, 483),
-		image.Rect(431, 498, 470, 574),
-		image.Rect(230, 665, 320, 715),
-		image.Rect(76, 653, 156, 700),
-		image.Rect(667, 496, 702, 548),
-		image.Rect(625, 500, 650, 547),
-		image.Rect(135, 783, 171, 870),
-		image.Rect(1, 878, 28, 939),
-	}
-
-	threshold := 98.50
-	// threshold := 39.0
-
-	gc := draw2dimg.NewGraphicContext(output)
-
-	emptyPixel := color.RGBA{R: 0xff, G: 0xff, B: 0xff, A: 0xff}
-	for i, rect := range checkRects {
-		rectImage := edgesImg.SubImage(rect)
-
-		// if i == 30 {
-		// 	rectFile, err := os.Create("test_rect.png")
-		// 	if err != nil {
-		// 		panic(err.Error())
-		// 	}
-
-		// 	png.Encode(rectFile, rectImage)
-
-		// 	rectFile.Close()
-		// }
-
-		emptyPixelCount := 0.0
-		notEmptyPixelCount := 0.0
-
-		utils.ParallelForEachPixel(rectImage.Bounds().Size(), func(x int, y int) {
-			pixel := rectImage.At(rectImage.Bounds().Min.X+x, rectImage.Bounds().Min.Y+y)
-			if pixel != emptyPixel {
-				// fmt.Printf("%dx%d, %#v\n", x, y, pixel)
-				notEmptyPixelCount++
-			} else {
-				emptyPixelCount++
+				pixelGray := imgEdges.At(x, y)
+				if pixelGray != emptyPixel {
+					// img.Set(x, y, color.RGBA{255, 0, 0, 255})
+					polygon.IncNonZero()
+				} else {
+					polygon.IncZero()
+				}
 			}
-		})
+		}
+	})
 
-		emptyPercent := 100 - ((notEmptyPixelCount / emptyPixelCount) * 100)
-		// fmt.Println(i, emptyPixelCount, notEmptyPixelCount, emptyPercent)
+	for _, polygon := range polygons {
+		if polygon.Zero != 0 {
+			center := polygon.Center()
+			percentage := 100 - float64(polygon.NonZero)/float64(polygon.Zero)*100
 
-		if emptyPercent > threshold {
-			gc.SetStrokeColor(color.RGBA{0, 255, 0, 255})
-			draw2dkit.Rectangle(gc, float64(rect.Min.X), float64(rect.Min.Y), float64(rect.Max.X), float64(rect.Max.Y))
-			gc.Stroke()
-
-			addLabel(output, rect.Min.X+2, rect.Max.Y-2, fmt.Sprintf("%d %.0f%%", i+1, emptyPercent), color.RGBA{0, 255, 0, 255})
-		} else {
-			gc.SetStrokeColor(color.RGBA{255, 0, 0, 255})
-			draw2dkit.Rectangle(gc, float64(rect.Min.X), float64(rect.Min.Y), float64(rect.Max.X), float64(rect.Max.Y))
-			gc.Stroke()
-			addLabel(output, rect.Min.X+2, rect.Max.Y-2, fmt.Sprintf("%d %.0f%%", i+1, emptyPercent), color.RGBA{255, 0, 0, 255})
+			// fmt.Println(i, "Percentage:", percentage, "%")
+			if percentage > tresholdEmpty {
+				// if percentage > 33 {
+				DrawPolygon(imgRGBA, polygon, color.RGBA{0, 255, 0, 255})
+				DrawLabel(imgRGBA, int(center.X), int(center.Y), fmt.Sprintf("%.1f", percentage), color.RGBA{0, 255, 0, 255})
+			} else {
+				DrawLabel(imgRGBA, int(center.X), int(center.Y), fmt.Sprintf("%.1f", percentage), color.RGBA{255, 0, 0, 255})
+				// DrawPolygon(img, polygon, color.RGBA{255, 0, 0, 255})
+			}
 		}
 	}
 
-	// draw2dimg.SaveToPngFile("result.png", output)
-	// draw2dimg.SaveToPngFile("edgesImg.png", edgesImg)
+	fmt.Printf("took %s\n", time.Since(start))
 
 	chatID, err := strconv.ParseInt(r.FormValue("target"), 10, 64)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+
 		return
 	}
 
-	isUpdate := true
+	isUpdate := false
 
 	if r.FormValue("update") == "1" {
 		isUpdate = true
@@ -244,27 +179,28 @@ func processImage(w http.ResponseWriter, r *http.Request) {
 		messageID := r.FormValue("message_id")
 		threadID := r.FormValue("thread_id")
 
-		sendImageUpdateToTelegram(output, chatID, messageID, threadID, token)
+		sendImageUpdateToTelegram(imgRGBA, chatID, messageID, threadID, token)
 
 		return
 	}
 
-	sendImageTotelegram(output, chatID, token)
+	sendImageTotelegram(imgRGBA, chatID, token)
+
 }
 
 func sendImageTotelegram(img image.Image, chatID int64, botToken string) {
-	file, err := os.Create("output.jpg")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
+	// file, err := os.Create("output.jpg")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer file.Close()
 
-	err = jpeg.Encode(file, img, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// err = jpeg.Encode(file, img, nil)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendPhoto?chat_id=%d", botToken, chatID)
 
@@ -275,28 +211,28 @@ func sendImageTotelegram(img image.Image, chatID int64, botToken string) {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(string(body))
+	// fmt.Println(string(body))
 }
 
 func sendImageUpdateToTelegram(img image.Image, chatID int64, messageID, threadID, botToken string) {
-	file, err := os.Create("output.jpg")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
+	// file, err := os.Create("output.jpg")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer file.Close()
 
-	err = jpeg.Encode(file, img, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// err = jpeg.Encode(file, img, nil)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
 	replyKeyboard := "reply_markup=%7B%22inline_keyboard%22%3A%20%5B%5B%7B%22text%22%3A%20%22Update%20🤓%22%2C%22callback_data%22%3A%20%22%2Fcamera_update%22%7D%5D%5D%7D&media=%7B%22type%22%3A%20%22photo%22%2C%20%22media%22%3A%22attach%3A%2F%2Fphoto%22%7D"
 
@@ -316,13 +252,13 @@ func sendImageUpdateToTelegram(img image.Image, chatID int64, messageID, threadI
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(string(body))
+	// fmt.Println(string(body))
 }
 
 func upload(url string, img image.Image) (*http.Response, error) {
@@ -358,20 +294,51 @@ func upload(url string, img image.Image) (*http.Response, error) {
 	}
 
 	fmt.Println(resp.Status)
-	fmt.Println(resp.Header)
-	fmt.Println(resp.Body)
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("bad status: %s, %s", resp.Status, resp.Body)
+	}
 
 	return resp, nil
 }
 
-func addLabel(img *image.RGBA, x, y int, label string, col color.RGBA) {
-	point := fixed.Point26_6{X: fixed.I(x), Y: fixed.I(y)}
+// func addLabel(img *image.RGBA, x, y int, label string, col color.RGBA) {
+// 	point := fixed.Point26_6{X: fixed.I(x), Y: fixed.I(y)}
 
-	d := &font.Drawer{
-		Dst:  img,
-		Src:  image.NewUniform(col),
-		Face: basicfont.Face7x13,
-		Dot:  point,
+// 	d := &font.Drawer{
+// 		Dst:  img,
+// 		Src:  image.NewUniform(col),
+// 		Face: basicfont.Face7x13,
+// 		Dot:  point,
+// 	}
+// 	d.DrawString(label)
+// }
+
+func formatForm(r *http.Request) string {
+	// Таблица значений переменных
+	varTable := map[string]string{
+		"target": r.FormValue("target"),
+		"token":  r.FormValue("token"),
+		"day":    r.FormValue("day"),
 	}
-	d.DrawString(label)
+
+	// Функция замены, подставляет значение переменной из таблицы varTable
+	substitutor := func(match string) string {
+		// match - значение вида `${var_name}`
+		// сначала извлечём var_name
+		varName := match[2 : len(match)-1]
+		// Теперь получим значение из таблицы
+		value, ok := varTable[varName]
+		if !ok {
+			// один из вариантов обработки отсутствующего значения - вернуть пустую строку
+			value = ""
+		}
+		return value
+	}
+
+	// Регулярное выражение для поиска строк вида
+	// 'начинается с ${, затем любые символы кроме { и }
+	// в любом количестве, заканчивается }'
+	re := regexp.MustCompile(`\${[^{}]*}`)
+
+	return re.ReplaceAllStringFunc(formTemplate, substitutor)
 }
---
+++ poly/raycast.go
@@ -0,0 +1,105 @@
+// Raycast shows an implementation of the ray casting point-in-polygon
+// (PNPoly) algorithm for testing if a point is inside a closed polygon.
+// Also known as the crossing number or the even-odd rule algorithm.
+//
+// The implementation follows
+// https://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+package poly
+
+// XY is a 2D point in the Cartesian plane.
+type XY struct {
+	X, Y float64
+}
+
+// Poly represents a closed polygon.  Pairs of consecutive points represent
+// endpoints of segments.  The last and first point represent an additional
+// segment.  That is, the last point does not need to repeat the first to
+// close the polygon.
+type Poly struct {
+	XY            []XY
+	Zero, NonZero int
+}
+
+func (pt *Poly) IncZero() {
+	pt.Zero++
+}
+
+func (pt *Poly) IncNonZero() {
+	pt.NonZero++
+}
+
+// In returns true if pt is inside pg.
+//
+// Segments of the polygon are allowed to cross.  In this case they divide the
+// polygon into multiple regions.  The function returns true for points in
+// regions on the perimeter of the polygon.  The return value for interior
+// regions is determined by a two coloring of the regions.
+//
+// If pt is exactly on a segment or vertex of pg, the method may return true or
+// false.
+func (pt XY) In(pg Poly) bool {
+	if len(pg.XY) < 3 {
+		return false
+	}
+	a := pg.XY[0]
+	in := rayIntersectsSegment(pt, pg.XY[len(pg.XY)-1], a)
+	for _, b := range pg.XY[1:] {
+		if rayIntersectsSegment(pt, a, b) {
+			in = !in
+		}
+		a = b
+	}
+	return in
+}
+
+// Segment intersect expression from
+// https://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+//
+// Currently the compiler inlines the function by default.
+func rayIntersectsSegment(p, a, b XY) bool {
+	return (a.Y > p.Y) != (b.Y > p.Y) &&
+		p.X < (b.X-a.X)*(p.Y-a.Y)/(b.Y-a.Y)+a.X
+}
+
+func (pt *Poly) Center() XY {
+	vertices := pt.XY
+	vertexCount := len(vertices)
+
+	centroid := XY{0, 0}
+	signedArea := 0.0
+	x0 := 0.0
+	y0 := 0.0
+	x1 := 0.0
+	y1 := 0.0
+	a := 0.0
+
+	// For all vertices except last
+	i := 0
+	for i < vertexCount-1 {
+		x0 = vertices[i].X
+		y0 = vertices[i].Y
+		x1 = vertices[i+1].X
+		y1 = vertices[i+1].Y
+		a = x0*y1 - x1*y0
+		signedArea += a
+		centroid.X += (x0 + x1) * a
+		centroid.Y += (y0 + y1) * a
+		i++
+	}
+
+	// Do last vertex separately to avoid performing an expensive modulus operation in each iteration.
+	x0 = vertices[i].X
+	y0 = vertices[i].Y
+	x1 = vertices[0].X
+	y1 = vertices[0].Y
+	a = x0*y1 - x1*y0
+	signedArea += a
+	centroid.X += (x0 + x1) * a
+	centroid.Y += (y0 + y1) * a
+
+	signedArea *= 0.5
+	centroid.X /= (6.0 * signedArea)
+	centroid.Y /= (6.0 * signedArea)
+
+	return centroid
+}
